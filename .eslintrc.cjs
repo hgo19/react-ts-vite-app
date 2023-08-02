7e67fd0ae16496ed9de101b4dfcd4d4032220d39
Hugo Leonardo <huleonardo19@gmail.com>
2023-08-02 16:39:09 -0300

    chore: config eslint with airbnb plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,12 +2,18 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'ES2020',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
